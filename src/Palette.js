3ff6a417720d8a364523d809f282038e8510e573
Teemamin <teemamin@yahoo.com>
2021-05-27 12:17:29 +0400

    initial commit

---
+++ src/Palette.js
@@ -0,0 +1,19 @@
+import React,{Component} from 'react'
+
+class Palette extends Component{
+
+
+    render(){
+        return(
+            <div className='Palette'>
+                {/* Nabar goes here*/}
+                <div className='Palette-colors'>
+                     {/* bunch of color goes here*/}
+                </div>
+                {/* Footer goes here*/}
+            </div>
+        )
+    }
+}
+
+export default Palette
