5052d6fa955a02a099155ed3c2e5acb156fab17f
Teemamin <teemamin@yahoo.com>
2021-05-30 11:25:11 +0400

    added color helper to generate colors

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React,{Component} from 'react'
 import seedColors from './seedColors'
 import Palette from './Palette'
+import { generatePalette } from './colorHelpers'
 
 class App extends Component {
 
   render(){
+    console.log(generatePalette(seedColors[3]))
       return (
         <div className="">
           <Palette {...seedColors[3]}/>
--- src/ColorBox.js
+++ src/ColorBox.js
@@ -1,24 +1,41 @@
 import React,{Component} from 'react'
 import './ColorBox.css'
+import {CopyToClipboard} from 'react-copy-to-clipboard';
 
 class ColorBox extends Component{
+    state={
+        copied: false
+    }
 
-
+    changeCOpyState = ()=>{
+        this.setState({copied:true},()=>{
+            setTimeout(()=>this.setState({copied:false}),1500)
+        })
+    }
     render(){
         let style = {
             background : this.props.background
         }
         const name = this.props.name
+        const {copied} = this.state
         return(
-            <div className='ColorBox' style={style}>
-                <div className='copy-container'>
-                    <div className='box-content'>
-                        <span>{name}</span>
-                    </div>
-                    <button className='copy-button'>Copy</button>
+            <CopyToClipboard text={this.props.background} onCopy={this.changeCOpyState}>
+                <div className='ColorBox' style={style}>
+                    {/* if this.state.copied is true: add the class show else just the overlsy class */}
+                <div className={`copy-overlay ${copied && 'show'}`} style={style}/>
+                <div className={`copy-msg ${copied && 'show'}`}>
+                    <h1>Copied!</h1>
+                    <p>{this.props.background}</p>
                 </div>
-                <span className='see-more'>More</span>
-            </div>
+                    <div className='copy-container'>
+                        <div className='box-content'>
+                            <span>{name}</span>
+                        </div>
+                        <button className='copy-button'>Copy</button>
+                    </div>
+                    <span className='see-more'>More</span>
+                </div>
+            </CopyToClipboard>
         )
     }
 }
